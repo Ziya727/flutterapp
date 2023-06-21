32cbdc77b167772bd6f53ec9fbaf8b0c952cce73
Ziya727 <ziya.alizade000@gmail.com>
2023-06-21 18:51:59 +0400

    firebaseapp

---
+++ firebaseapp/android/app/src/main/kotlin/com/example/firebaseapp/firebaseapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firebaseapp.firebaseapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ firebaseapp/lib/screens/wrapper.dart
@@ -0,0 +1,12 @@
+import 'package:firebaseapp/screens/authenticate/authenticate.dart';
+import 'package:firebaseapp/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Authenticate();
+  }
+}
